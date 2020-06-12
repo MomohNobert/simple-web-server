3aeb576ddc7b91e1f75888a5686f317da0d49167
Momoh Nobert <okhai0112@gmail.com>
2020-06-12 17:24:17 +0100

    checking to see if it was a file

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 1337;
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('I have to start again, REFRESH\n');
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+http.createServer(function(req, res) {
+  var uri = url.parse(req.url).pathname;
+  var filename = path.join(process.cwd(), unescape(uri));
+  console.log('Loading ' + uri);
+  var stats;
 });
+
+// const hostname = '127.0.0.1';
+// const port = 1337;
+
+// http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('I have to start again, REFRESH\n');
+// }).listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
